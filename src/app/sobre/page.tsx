ae4a283f5b424bd905d380e2719fec2f2916da86
Guilherme Bastos Leone <guilhermebastosleone@gmail.com>
2023-12-30 13:45:05 -0300

    Add image support and update styles

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,9 +1,46 @@
-import Image from 'next/image'
+import Image from "next/image";
+import styles from "./about.module.css";
 
-export default function AboutPage() {
+export const metadata = {
+  title: "Sobre nós",
+  description: "Sobre nós",
+};
+
+export default function AboutPage () {
+
   return (
-    <div>
-      <Image src="/about.png" alt='Sobre imagem' width={200} height={200} />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>Sobre Nós</h2>
+        <h1 className={styles.title}>
+        Desenvolvemos ideias digitais maiores, mais ousadas, corajosas e melhores.
+        </h1>
+        <p className={styles.desc}>
+        Criamos ideias digitais que são maiores, mais ousadas, mais corajosas e melhores. Acreditamos na flexibilidade e precisão das boas ideias. Somos a melhor equipe de consultoria e soluções financeiras do mundo. Oferecemos uma ampla gama de serviços de desenvolvimento de web e software.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Anos de experiência</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Anos de experiência</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Anos de experiência</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/about.png"
+          alt="About Image"
+          fill
+          className={styles.img}
+        />
+      </div>
     </div>
-  )
+  );
 }
